1168dcc8be020da0b3c0df3012b5e7ce79583129
Nazish Khan <knazish1122@gmail.com>
2024-02-23 16:53:05 +0000

    commit

--- R/transformation.R
+++ R/transformation.R
@@ -1,10 +1,13 @@
 library(RSQLite)
 library(readr)
-
+print("Reading the data")
 # Reading the csv file
-product_data <- readr::read_csv("data_upload/MOCK_DATA_PRODUCTS.csv")
+product_data <- readr::read_csv("data_upload/MOCK_DATA_PRODUCTS.csv",show_col_types = FALSE)
 
 # Writing the data to the database
+print("Writing to the database")
 my_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"database/products.db")
+RSQLite::dbRemoveTable(my_connection,"products")
 RSQLite::dbWriteTable(my_connection,"products",product_data)
-RSQLite::dbDisconnect()
+RSQLite::dbDisconnect(my_connection)
+print("Done")
